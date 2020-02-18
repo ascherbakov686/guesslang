81eb87fa15369fc13c5832c7f3efe540031050ef
Alexey Shcherbakov <alexey.shcherbakov@kaspersky.com>
2020-02-18 17:51:55 +0300

    script get_list_sites

--- tools/get_links_from_file.py
+++ tools/get_links_from_file.py
@@ -2,10 +2,23 @@
 
 from bs4 import BeautifulSoup
 import glob
+#from urllib.request import urlopen
+from requests_html import HTMLSession
+from urllib.parse import urlsplit
+from re import compile
 
 for f in glob.glob("../datasets/list_of_sites/drupal/*.html"):
- print(f)
  soup = BeautifulSoup(open(f), 'html.parser')
  par = soup.find_all('a', href=True)
+ rex = compile('.*google.*')
+
  for a in par:
-   print(a['href'])
+   if not rex.match(str(a)):
+     base_url = "{0.scheme}://{0.netloc}/".format(urlsplit(a['href']))
+     if base_url != ':///':
+      session = HTMLSession()
+      r = session.get(base_url)
+      r.html.render()
+      with open('../datasets/learn/%s.html.drupal' % urlsplit(a['href']).netloc, 'w', encoding='utf-8') as f_out:
+         f_out.write(r.html.raw_html)
+         f_out.close()
