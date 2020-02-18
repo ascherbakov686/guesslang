d56c24624cd92a13a51324d4fc18ecb1ac26a875
Alexey Shcherbakov <alexey.shcherbakov@kaspersky.com>
2020-02-18 18:10:59 +0300

    script get_list_sites

--- tools/get_links_from_file.py
+++ tools/get_links_from_file.py
@@ -7,6 +7,9 @@ from requests_html import HTMLSession
 from urllib.parse import urlsplit
 from re import compile
 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
 for f in glob.glob("../datasets/list_of_sites/drupal/*.html"):
  soup = BeautifulSoup(open(f), 'html.parser')
  par = soup.find_all('a', href=True)
@@ -17,8 +20,7 @@ for f in glob.glob("../datasets/list_of_sites/drupal/*.html"):
      base_url = "{0.scheme}://{0.netloc}/".format(urlsplit(a['href']))
      if base_url != ':///':
       session = HTMLSession()
-      r = session.get(base_url)
-      r.html.render()
+      r = session.get(base_url,verify=False)
       with open('../datasets/learn/%s.html.drupal' % urlsplit(a['href']).netloc, 'w', encoding='utf-8') as f_out:
-         f_out.write(r.html.raw_html)
+         f_out.write(r.html.html)
          f_out.close()
